870213940b79271b47ace7b1894a036bd8f2740d
asohyannick <asohyannick3@gmail.com>
2024-05-22 19:27:09 -0700

    completed the login API endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import mongoose from 'mongoose';
 import 'dotenv/config';
 import userRoute from './routes/users';
+import authRoute from './routes/auth';
 mongoose.connect(process.env.MONGO_URI as string);
 const app:Application = express();
 app.use(express.json());
@@ -11,6 +12,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cors());
 app.use(cookieParser());
 const port = process.env.PORT || 8000;
+app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/users', userRoute);
 app.listen(() => { 
     console.log(`Server is running on port ${port}...`);
